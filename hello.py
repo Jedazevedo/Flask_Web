42a717ae90c3b2e2ac8afeb0f84d71afe83fb6c7
jedson Ferreira <jedazevedo@gmail.com>
2024-08-24 23:14:37 -0300

    Atualizanção do arquivo base

--- hello.py
+++ hello.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+@app.route("/sobre")
+def bye():
+    return "Hello, World!"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
