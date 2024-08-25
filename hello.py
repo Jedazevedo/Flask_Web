8b2821664fb62966e5479d3d43366f6c82b722d6
jedson Ferreira <jedazevedo@gmail.com>
2024-08-25 14:38:48 -0300

    inclusão de arquivo static

--- hello.py
+++ hello.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template, session, redirect, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
 
 
 @app.route("/sobre")
-def bye():
-    return "Hello, World!"
+def sobre():
+    return "TELA SOBRE SO NOSSO APP!"
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
 
+#static files  
+    
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
+# venv ativado
+# debug ativado sozinho, use o comando: flask --app hello.py run --debug
